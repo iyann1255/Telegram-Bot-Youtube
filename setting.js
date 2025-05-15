347a55d5f1f7b907de5966e0d5a56445b7da4e94
iyann1255 <tessbarca1981@gmail.com>
2025-05-15 22:17:30 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tokenBot: '', // your bot token
+  tokenBot: '7602789834:AAGYEktSlfTtDQWidTu8XnjJyeuv76UdwNM', // your bot token
   apikey: 'free', // default global, zex, or free
   miftahapi: 'https://api.miftahganzz.my.id/api/'
 };
